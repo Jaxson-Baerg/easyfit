46853656e02985ca82d2a438b839f5659a6adc22
Sam <samjulian6@gmail.com>
2022-11-26 14:45:05 -0800

    Remove redundant display route on backend

--- server/app.js
+++ server/app.js
@@ -4,7 +4,6 @@ const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
-const indexRouter = require('./routes/index');
 const studentsRouter = require('./routes/students');
 
 const app = express();
