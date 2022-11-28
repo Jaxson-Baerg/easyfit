3faa841538d42376cdb783915c2e61979cf1b980
Sam <samjulian6@gmail.com>
2022-11-28 11:21:16 -0800

    Add initial components and enable react router

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import { Route, Routes } from 'react-router-dom'
+import NavBar from './components/NavBar';
+import AllClassTypes from './components/AllClassTypes';
 
 const App = () => {
+  const [optionsActive, setOptionsActive] = useState(false);
+
   return (
-    <h2>Welcome to EasyFit!</h2>
+    <>
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<AllClassTypes/>}/>
+      </Routes>
+    </>
   );
 };
 
---
+++ client/src/components/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import NavButton from "./NavButton";
+
+export default function NavBar(props) {
+   return (
+      <nav>
+        <h1>EasyFit</h1>
+        <NavButton />
+      </nav>
+   );
+};
