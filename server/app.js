5372485b9f8102f4d33bcc7ca9b7730bdd363072
Sam <samjulian6@gmail.com>
2022-11-28 19:10:15 -0800

    Add route to handle payment on backend

--- server/app.js
+++ server/app.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const createError = require('http-errors');
 const express = require('express');
 const path = require('path');
@@ -10,6 +11,10 @@ const classTypesRouter = require('./routes/class_types');
 const classStudentsRouter = require('./routes/class_students');
 
 const app = express();
+
+const stripe = require("stripe")(process.env.STRIPE_SECRET_TEST);
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -40,6 +45,38 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.post("/payment", cors(), async (req, res) => {
+	let { amount, id } = req.body
+	try {
+		const payment = await stripe.paymentIntents.create({
+			amount,
+			currency: "USD",
+			description: "Spatula company",
+			payment_method: id,
+			confirm: true
+		})
+		console.log("Payment", payment)
+		res.json({
+			message: "Payment successful",
+			success: true
+		})
+	} catch (error) {
+		console.log("Error", error)
+		res.json({
+			message: "Payment failed",
+			success: false
+		})
+	}
+});
+
+app.listen(process.env.PORT || 4000, () => {
+	console.log("Sever is listening on port 4000")
 });
 
 module.exports = app;
