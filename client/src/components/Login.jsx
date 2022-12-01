57a44623357250909a691da39eb52db8744bbd5a
Sam <samjulian6@gmail.com>
2022-12-01 15:54:48 -0800

    Add components for student login/register

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -2,6 +2,20 @@ import React from 'react'
 
 export default function Login() {
   return (
-    <div>Login</div>
+    <div className="container">
+              <div>
+                <article>
+                  <h4>Login</h4>
+                  <p>Please enter your email</p>
+                  <form>
+                    <div>
+                      <span> <i className="fa fa-envelope"></i> </span>
+                      <input name="email" placeholder="Email address" type="email" />
+                    </div>
+                    <p>Don't have an account? <a href="/register">Log In</a> </p>
+                  </form>
+                </article>
+              </div>
+            </div>
   )
 }
--- client/src/components/Register.jsx
+++ client/src/components/Register.jsx
@@ -2,6 +2,25 @@ import React from 'react'
 
 export default function Register() {
   return (
-    <div>Register</div>
+          <div className="container">
+              <div>
+                <article>
+                  <h4>Create Account</h4>
+                  <p>Get started with your free account</p>
+                  <form>
+                    <div>
+                      <span><i className="fa fa-user"></i></span>
+                      <input name="first_name" placeholder="First Name" type="text" />
+                      <input name="last_name" placeholder="Last Name" type="text" />
+                    </div>
+                    <div>
+                      <span> <i className="fa fa-envelope"></i> </span>
+                      <input name="email" placeholder="Email address" type="email" />
+                    </div>
+                    <p>Have an account? <a href="/login">Log In</a> </p>
+                  </form>
+                </article>
+              </div>
+            </div>
   )
 }
