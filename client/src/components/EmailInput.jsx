5809883368fe292789a3f938d75bae8d7a8a13d6
Jaxson-Baerg <jaxson.baerg@gmail.com>
2022-12-02 18:24:04 -0700

    Seperate login forms and create logic

---
+++ client/src/components/EmailInput.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+
+import '../styles/css/EmailInput.css';
+
+export default function EmailInput(props) {
+  const [email, setEmail] = useState('');
+
+  return (
+    <div className='emailinput'>
+      <article>
+        <h4>Login</h4>
+        <p>Please enter your email</p>
+        <form action="http://localhost:3001/login" method="post" onSubmit={props.submitHandler}>
+          <div>
+          <span> <i className="fa fa-envelope"></i> </span>
+          <input name="email" placeholder="Email address" type="email" value={email} onChange={event => setEmail(event.target.value)}/>
+          </div>
+          <button type='submit'>Submit</button>
+          <p>Don't have an account? <a href="/register">Register</a> </p>
+        </form>
+      </article>
+    </div>
+  );
+}
