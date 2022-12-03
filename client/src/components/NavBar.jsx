a2d94f112706beb441a687cbe7cbd9f4f4c6320e
Jaxson-Baerg <jaxson.baerg@gmail.com>
2022-12-02 18:23:13 -0700

    Add visual indicator of being logged in as well as logout button

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,20 +1,31 @@
-import React, { useRef } from "react";
-import { Link } from "react-router-dom";
-import '../styles/css/NavBar.css';
+import React, { useState, useEffect, useRef } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 import NavList from "./NavList";
 
-export default function NavBar(props) {
-   const navOptions = useRef(null);
+import '../styles/css/NavBar.css';
 
-   return (
-      <nav className="wave">
-        <div>
-          <h1><Link to="/" style={{all: "unset"}}> EasyFit </Link></h1>
-          <img src={require('../images/easyfit-logo.png')} alt='EasyFit' width={"50"} height={"50"}/>
-        </div>
-        <NavList navOptions={navOptions}/>
-        <button onClick={() => navOptions.current.classList.toggle('show')}><i className="fa-solid fa-bars fa-2x"></i></button>
-      </nav>
+export default function NavBar(props) {
+  const [studentName, setStudentName] = useState(null);
+  const navOptions = useRef(null);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    axios.get(`/students/${props.loggedInId}`)
+      .then(result => setStudentName(result.data[0].first_name))
+      .catch(e => {});
+  }, [props.loggedInId]);
+
+  return (
+    <nav className="wave">
+      <div>
+        <h1><Link to="/" style={{all: "unset"}}> EasyFit </Link></h1>
+        <img src={require('../images/easyfit-logo.png')} alt='EasyFit' width={"50"} height={"50"}/>
+        {props.loggedInId && <><h4>Logged in as: {studentName}</h4><button onClick={() => {props.logout(); navigate('/'); window.location.reload(false);}}>Logout</button></>}
+      </div>
+      <NavList navOptions={navOptions}/>
+      <button onClick={() => navOptions.current.classList.toggle('show')}><i className="fa-solid fa-bars fa-2x"></i></button>
+    </nav>
    );
 };
