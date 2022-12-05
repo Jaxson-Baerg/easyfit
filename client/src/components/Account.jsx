9d63d646fb88ce33e26a37c139f2cd026955bcf4
Sam <samjulian6@gmail.com>
2022-12-05 11:22:13 -0800

    Small fix to time conversion from 24 hour to AM/PM

--- client/src/components/Account.jsx
+++ client/src/components/Account.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Cookies from 'universal-cookie';
 import axios from 'axios';
+import timeConvert from '../helpers/timeConvert';
 
 import '../styles/css/Account.css';
 
@@ -18,7 +19,7 @@ export default function Account() {
     };
     const formatTime = (dateStr) => {
       const tempDate = dateStr.split(/[-T.]+/);
-      return `${tempDate[3].split(':').slice(0, 2).join(':')}${Number(tempDate[3].split(':')[0]) > 11 || Number(tempDate[3].split(':')[0]) === 24 ? "pm" : "am"}`;
+      return timeConvert(tempDate[3]);
     };
 
     const cancelRegistration = (class_id) => {
